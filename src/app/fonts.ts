31f7c580fda54a8cf971c6dc3c4925a7cbb6656a
EN-hyeoninchoi <hyeonin.choi@enlighten.kr>
2024-11-15 17:58:42 +0900

    feat: 스타일, 폰트 초기 설정

---
+++ src/app/fonts.ts
@@ -0,0 +1,17 @@
+import localFont from 'next/font/local'
+
+export const pretendard = localFont({
+    src: [
+        {
+            path: './fonts/Pretendard-Regular.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: './fonts/Pretendard-Bold.woff2',
+            weight: '700',
+            style: 'normal',
+        },
+    ],
+});
+
